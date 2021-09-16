2d41c733531bcdbe62099bbc57fd0468258ddfcd
Rob Culliton <rob.culliton@gmail.com>
2021-09-16 08:57:20 -0400

    continuing to work out good-enough?

--- core.lisp
+++ core.lisp
@@ -1,29 +1,36 @@
 (defvar yale-model
-  '((us-equities 30)
-    (foreign-equities 15)
-    (emerging-equities 5)
-    (inter-treasuries 15)
-    (tips 15)
-    (reits 20)))
+  '((us-equities .30 178)
+    (foreign-equities . .15)
+    (emerging-equities . .5)
+    (inter-treasuries . .15)
+    (tips . .15)
+    (reits . .20)))
 
-(defvar all-weather
-  '((us-equities 30)
-    (inter-treasuires 15)
-    (long-treasuries 40)
-    (gold 7.5)
-    (commodities 7.5)))
 
 (defvar budget 10000)
 
-
 ;; where guesses is a list of dotteded pairs of (17 . us-equities)
-(defun good-enough? (guesses)
-  (and ))
+(defun good-enough? (guess)
+  (let* ((asset-meta (assoc (car guess) yale-model))
+         (percentage (cadr asset-meta))
+         (price (caddr asset-meta))
+        (maximum (/ (* budget percentage) price)))
+    (values
+     (* percentage
+        (cdr guess))
+     maximum)))
+
+(good-enough? '(us-equities . 17))
+
+(cdr (assoc 'us-equities yale-model))
 
 
-
-
-
+(defvar guesses '((us-equities . 17)
+                  (foreign-equities . 6)
+                  (emerging-equities . 4)
+                  (inter-treasuries . 12)
+                  (tips . 8)
+                  (reits . 24)))
 
 
 
