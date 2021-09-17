e76162acc0f6e453a1bd1ad57e04aa964e5a1ed7
Rob Culliton <rob.culliton@gmail.com>
2021-09-17 15:33:15 -0400

    getting warmer!

--- core.lisp
+++ core.lisp
@@ -10,22 +10,23 @@
       0
       (/ budget (* (etf-num e) (etf-price e)))))
 
-(defvar budget 1000)
+(defmethod far-off (e)
+  (- (etf-weight e) (current-weight e)))
 
-(defvar emerging (make-etf :weight .05
-                                   :num 12
-                                   :price 64))
+(defvar budget 100)
 
-(defparameter us-equities (make-etf :weight .30
+(defvar emerging (make-etf :weight .30
+                                   :num 0
+                                   :price 10))
+
+(defparameter us-equities (make-etf :weight .70
                                      :num 0
-                                     :price 172))
-
-
+                                     :price 10))
 
 (defun improve (best-so-far fund)
   (cond ((null fund)
          best-so-far)
-        ((> (current-weight best-so-far) (current-weight (car fund)))
+        ((> (far-off best-so-far) (far-off (car fund)))
          (improve best-so-far (cdr fund)))
         (t (improve (car fund) (cdr fund)))))
 
@@ -33,5 +34,3 @@
   (improve (car fund) (cdr fund)))
 
 (next-pick (list emerging us-equities))
-
-
