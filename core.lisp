3d39dfa2851a5b454a2fbbd0293cbe6d9fd063e0
Rob Culliton <rob.culliton@gmail.com>
2021-09-19 07:49:39 -0400

    this might be what we need, questions of state and scope remain

--- core.lisp
+++ core.lisp
@@ -5,15 +5,22 @@
   num
   price)
 
+(defvar budget 100)
+
+(defmethod expenditure (e)
+  (* (etf-num e) (etf-price e)))
+
+
 (defmethod current-weight (e)
   (if (zerop (etf-num e))
       0
-      (/ budget (* (etf-num e) (etf-price e)))))
+      (/ budget (expenditure e))))
 
 (defmethod far-off (e)
   (- (etf-weight e) (current-weight e)))
 
-(defvar budget 100)
+(defun total-expenditure (fund)
+  (apply #'+ (mapcar #'expenditure fund)))
 
 (defvar emerging (make-etf :weight .30
                                    :num 0
@@ -35,7 +42,13 @@
 
 (defun allocate (fund budget)
   ;; TODO here
-  (next-pick fund))
+  ;; make a data structure of etfs and incremented counts
+  ;; dec budget
+  (if (> budget (total-expenditure fund))
+      (let ((pick (next-pick fund)))
+        (incf (etf-num pick))
+        (allocate fund (- budget (etf-price pick))))
+      fund))
 
 (allocate (list emerging us-equities) budget)
 
