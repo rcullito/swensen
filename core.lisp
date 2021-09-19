8cdbb211c5ffa773bd603378cbebcecbdd3d256a
Rob Culliton <rob.culliton@gmail.com>
2021-09-19 08:28:06 -0400

    getting warmer, great call to have our etfs be functions

--- core.lisp
+++ core.lisp
@@ -5,13 +5,15 @@
   num
   price)
 
-(defvar emerging (make-etf :weight .30
-                                   :num 0
-                                   :price 10))
+(defun emerging ()
+    (make-etf :weight .30
+              :num 0
+              :price 10))
 
-(defparameter us-equities (make-etf :weight .70
-                                     :num 0
-                                     :price 10))
+(defun us-equities ()
+  (make-etf :weight .70
+            :num 0
+            :price 10))
 
 (defmethod expenditure (e)
   (* (etf-num e) (etf-price e)))
@@ -19,7 +21,7 @@
 (defmacro current-weight (e)
   `(if (zerop (etf-num ,e))
       0
-      (/ budget (expenditure ,e))))
+      (/ (expenditure ,e) budget)))
 
 (defmacro far-off (e)
   `(- (etf-weight ,e) (current-weight ,e)))
@@ -28,6 +30,7 @@
   (apply #'+ (mapcar #'expenditure fund)))
 
 (defun improve (best-so-far fund budget)
+  (print-object budget *standard-output*)
   (cond ((null fund)
          best-so-far)
         ((> (far-off best-so-far) (far-off (car fund)))
@@ -41,13 +44,19 @@
   ;; TODO here
   ;; make a data structure of etfs and incremented counts
   ;; dec budget
-  (if (> budget (apply #'max (mapcar #'etf-price fund)))
+  (if (< budget (apply #'max (mapcar #'etf-price fund)))
+      fund
       (let ((pick (next-pick fund budget)))
         (incf (etf-num pick))
-        (allocate fund (- budget (etf-price pick))))
-      fund))
+        (allocate fund (- budget (etf-price pick))))))
 
-(allocate (list emerging us-equities) 100)
+(allocate (list (emerging) (us-equities)) 100)
+
+(let ((budget 100))
+  (current-weight emerging))
+
+emerging
+
 
 
 
